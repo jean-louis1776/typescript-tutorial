6f961cfcd2a4c0833bfb37022ffaa58fee43049f
ILYA <aleksin095@gmail.com>
2021-12-30 20:26:53 +0300

    lesson4

---
+++ sandbox.ts
@@ -0,0 +1,36 @@
+// ARRAYS
+let names = ["Luigi", "Mario", "Yoshi"]
+
+names.push("Toad")
+// names.push(3)
+// names[0] = 3
+
+let numbers = [10, 20, 30, 40]
+
+numbers.push(25)
+// numbers.push('Shaun')
+// numbers[1] = 'Shaun'
+
+let mixed = ["Ken", 4, "Chang-Chi", 8, 9]
+
+mixed.push("Ryu")
+mixed.push(10)
+mixed[0] = 3
+
+// OBJECTS
+let ninja = {
+    name: "Mario",
+    belt: "red",
+    age: 30,
+}
+
+ninja.age = 40
+ninja.name = "Ryu"
+// ninja.age = '30'
+// ninja.skills = ['fighting', 'sneaking']
+
+ninja = {
+    name: "Luigi",
+    belt: "green",
+    age: 40,
+}
