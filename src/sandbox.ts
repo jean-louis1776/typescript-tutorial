173ee8f39b11b0d21b6456aa2431f56a7f9d1ef0
ILYA <aleksin095@gmail.com>
2022-01-01 02:10:38 +0300

    lesson7

---
+++ src/sandbox.ts
@@ -0,0 +1,19 @@
+let greet: Function
+
+// greet = "hello"
+greet = () => {
+    console.log("hello, again")
+}
+
+const add = (a: number, b: number, c: number | string = 10) => {
+    console.log(a + b)
+    console.log(c)
+}
+
+add(5, 10, "20")
+
+const minus = (a: number, b: number) => {
+    return a + b
+}
+
+let result = minus(10, 7)
