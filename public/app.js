7c25895929895ff5ac8102ef2688676687ad4296
ILYA <aleksin095@gmail.com>
2022-01-01 23:12:32 +0300

    lesson15

--- public/app.js
+++ public/app.js
@@ -1,4 +1,19 @@
-// Classes
+const me = {
+    name: "Shaun",
+    age: 30,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log("I spent", amount);
+        return amount;
+    },
+};
+const greetPerson = (person) => {
+    console.log("hello", person.name);
+};
+greetPerson(me);
+console.log(me);
 import { Invoice } from "./classes/Invoice.js";
 const invOne = new Invoice("Mario", "work on the Mario website", 250);
 const invTwo = new Invoice("Luigi", "work on the Luigi website", 300);
