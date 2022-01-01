5fd1dcce0a3a2b9cde65e43223178b46c35a7400
ILYA <aleksin095@gmail.com>
2022-01-01 21:34:18 +0300

    lesson13

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 "use strict";
 // Classes
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // readonly client: string
+    // private details: string
+    // public amount: number
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
         return `${this.client} owes £${this.amount} for ${this.details}`;
@@ -15,8 +18,9 @@ const invTwo = new Invoice("Luigi", "work on the Luigi website", 300);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invOne.client = "Yoshi";
-invTwo.amount = 400;
+invoices.forEach((inv) => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 const form = document.querySelector(".new-item-form");
 // Inputs
 const type = document.querySelector("#type");
